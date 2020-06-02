37a83b05cd9c0b774905d7a1a03e3a647e8c1643
Palle Klewitz <palleklewitz@icloud.com>
2020-06-02 04:45:22 +0200

    fixes typo

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DL4STTensorboard",
+            name: "DL4STensorboard",
             targets: ["DL4STensorboard"]),
     ],
     dependencies: [
